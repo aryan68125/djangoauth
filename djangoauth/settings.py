97c70ef1041ea50c4e664e6c1430fdaec59c3207
aryan68125 <aryan68125@gmail.com>
2021-09-11 22:38:20 +0530

    send email

--- djangoauth/settings.py
+++ djangoauth/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'userpiyush6@gmail.com'
