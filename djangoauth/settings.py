f7160400a5c44b8b0525f195f3a385125a723959
aryan68125 <aryan68125@gmail.com>
2021-09-12 16:43:50 +0530

    made changes to my gmail account

--- djangoauth/settings.py
+++ djangoauth/settings.py
@@ -129,11 +129,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-relay.sendinblue.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'userpiyush6@gmail.com'
-EMAIL_HOST_PASSWORD = 'IKYhmn7HbGvUszZa'
+EMAIL_HOST_PASSWORD = 'rezwfjagyrxxmebx'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
